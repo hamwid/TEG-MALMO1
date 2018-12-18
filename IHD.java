4418245522afcbdf9cbfc433be4d609a53e9cf9a
hamwid <hampus.widell@alleman.nu>
2018-12-18 07:07:20 +0100

    Updated execute() and mean()

--- IHD.java
+++ IHD.java
@@ -24,12 +24,12 @@ public class IHD {
   public void execute(){
     this.listenToSensor();
     this.readFromSensor();
+    this.mean();
   }
 
   //Calculate mean
-  private int mean(int i1, int i2){
-    mean = (i1+i2)/2;
-    return mean;
+  private void mean(){
+    mean = (read1+read2)/2;
   }
 
   //Obtain mean between read1 and read2
